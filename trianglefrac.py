72af5066d0ecfd98b0464642328d43c8830173a8
Kenan Al-Shamie <kenan.mth@gmail.com>
2019-09-09 10:06:00 +0100

    Update trianglefrac.py
    comments
    function changes

--- trianglefrac.py
+++ trianglefrac.py
@@ -1,5 +1,5 @@
 import turtle
-PROGNAME = 'Sierpinski Triangle'
+PROGNAME = 'Sierpinski triangle'
 
 myPen = turtle.Turtle()
 myPen.speed(8)
@@ -7,10 +7,10 @@ myPen.pencolor('blue')
 # to define the three corners of the triangle
 points = [[-175,-125],[0,175],[175,-125]] 
 
-def getMid(p1,p2):
+def Midpoint(p1,p2):
      return ( (p1[0]+p2[0]) / 2, (p1[1] + p2[1]) / 2) 
 # recursively drawing smaller triangles
-def triangle(points,depth):
+def Sierpinski(points,depth):
 
      myPen.up()
      myPen.goto(points[0][0],points[0][1])
@@ -18,21 +18,21 @@ def triangle(points,depth):
      myPen.goto(points[1][0],points[1][1])
      myPen.goto(points[2][0],points[2][1])
      myPen.goto(points[0][0],points[0][1])
-
+     # for the function to include itself until depth isn't there
      if depth>0:
-          triangle([points[0],
-                        getMid(points[0], points[1]),
-                        getMid(points[0], points[2])],
+          Sierpinski([points[0],
+                        Midpoint(points[0], points[1]),
+                        Midpoint(points[0], points[2])],
                    depth-1)
-          triangle([points[1],
-                        getMid(points[0], points[1]),
-                        getMid(points[1], points[2])],
+          Sierpinski([points[1],
+                        Midpoint(points[0], points[1]),
+                        Midpoint(points[1], points[2])],
                    depth-1)
-          triangle([points[2],
-                         getMid(points[2], points[1]),
-                         getMid(points[0], points[2])],
+          Sierpinski([points[2],
+                         Midpoint(points[2], points[1]),
+                         Midpoint(points[0], points[2])],
                    depth-1)
      else:
           pass
 # to run the drawing
-triangle(points,6)
+Sierpinski(points,6)
